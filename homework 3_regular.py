b391b9ce7cf2285333c80a3ac03ccf4d30c3569d
MaxRevan <gladkovnissan@gmail.com>
2024-06-30 09:11:36 +0300

    com

--- homework 3_regular.py
+++ homework 3_regular.py
@@ -28,6 +28,6 @@ for phone_number in numbers_phone:
     
     print(normalized_number)
 
-
+#
 
     
--- homework_3_random.py
+++ homework_3_random.py
@@ -18,3 +18,4 @@ def get_numbers_ticket(min, max, quantity):
 lottery_numbers = get_numbers_ticket(1, 49, 6)
 print("Ваші лотерейні числа:", lottery_numbers)
 
+#
